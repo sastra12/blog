75480be2fd688ad91ab457602126553e17d6ed24
sastra12 <anonymousonly594@gmail.com>
2022-04-11 13:11:36 +0700

    first commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,378 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Icofont -->
+    <link rel="stylesheet" href="icofont/icofont.min.css">
+
+    <!-- AOS -->
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/base.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="site-container">
+        <nav>
+            <div class="logo">
+                <h2>NewsID.com</h2>
+            </div>
+            <div class="menu">
+                <a href="">News</a>
+                <a href="">Sport</a>
+                <a href="">Education</a>
+                {{-- <a href="">Economy</a>
+                <a href="">Entertainment</a>
+                <a href="">Government</a> --}}
+            </div>
+            <div class="mobile-menu">
+                <i class="icofont-navigation-menu"></i>
+            </div>
+        </nav>
+
+        <div class="container">
+            <div class="hero">
+                <div class="left content-box">
+                    <h4>Government</h4>
+                    <a href="detail.html">
+                        <h1>Warga Prancis Demo Tolak Kewajiban Kartu Vaksin</h1>
+                    </a>
+                    <p>Perancis akan segera memberlakukan wajib kartu vaksinasi di kereta api, penerbangan domestik, bus
+                        jarak jauh hingga kafe. Kebijakan ini ditentang sejumlah warga</p>
+                    <span>Jumat, 06 Agu 2021 09:10 WIB</span>
+                </div>
+                <div class="right">
+                    <img src="img/police.jpg" alt="">
+                </div>
+            </div>
+        </div>
+
+        <!-- Blog Section -->
+        <div class="container">
+            <div class="blog">
+                <h3 class="heading-category">Latest News</h3>
+                <div class="blog-post">
+                    <div class="thumbnail">
+                        <img src="img/meeting.jpg" alt="">
+                    </div>
+                    <div class="blog-content">
+                        <span>Education</span>
+                        <h3>Ojat Darojat Kembali Dilantik Jadi Rektor UT</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius dicta atque neque
+                            voluptatem accusantium aliquid eum sunt officiis cum illo!</p>
+                        <a href="">Read More</a>
+                    </div>
+                </div>
+
+                <div class="blog-post">
+                    <div class="thumbnail">
+                        <img src="img/drone.jpg" alt="">
+                    </div>
+                    <div class="blog-content">
+                        <span>Teknologi</span>
+                        <h3>Teknologi Drone</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius dicta atque neque
+                            voluptatem accusantium aliquid eum sunt officiis cum illo!</p>
+                        <a href="">Read More</a>
+                    </div>
+                </div>
+
+                <div class="blog-post">
+                    <div class="thumbnail">
+                        <img src="img/demo.jpg" alt="">
+                    </div>
+                    <div class="blog-content">
+                        <span>Government</span>
+                        <h3>Demo Protes Kartu Sehat Presiden Macron</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius dicta atque neque
+                            voluptatem accusantium aliquid eum sunt officiis cum illo!</p>
+                        <a href="">Read More</a>
+                    </div>
+                </div>
+
+                <div class="blog-post">
+                    <div class="thumbnail">
+                        <img src="img/marathon.jpg" alt="">
+                    </div>
+                    <div class="blog-content">
+                        <span>Sport</span>
+                        <h3>Hasil Lomba Maraton Olympiade Tokyo</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius dicta atque neque
+                            voluptatem accusantium aliquid eum sunt officiis cum illo!</p>
+                        <a href="">Read More</a>
+                    </div>
+                </div>
+
+                <div class="blog-post">
+                    <div class="thumbnail">
+                        <img src="img/football.jpg" alt="">
+                    </div>
+                    <div class="blog-content">
+                        <span>Sport</span>
+                        <h3>Jordi Alba Rela Gajinya Dipangkas Barcelona</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius dicta atque neque
+                            voluptatem accusantium aliquid eum sunt officiis cum illo!</p>
+                        <a href="">Read More</a>
+                    </div>
+                </div>
+
+                <div class="blog-post">
+                    <div class="thumbnail">
+                        <img src="img/money.jpg" alt="">
+                    </div>
+                    <div class="blog-content">
+                        <span>Economy</span>
+                        <h3>Uang Beredar Tembus Rp7.149,2 T di Juli 2021</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius dicta atque neque
+                            voluptatem accusantium aliquid eum sunt officiis cum illo!</p>
+                        <a href="">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Headlines Section -->
+        <div class="container">
+            <div class="headlines">
+                <h3 class="heading-category">Headlines</h3>
+                <div class="left" data-aos="fade-left" data-aos-delay="300" data-aos-duration="2000">
+                    <div class="thum">
+                        <img src="img/VR.jpg" alt="">
+                    </div>
+                    <div class="blog-content">
+                        <span>Teknologi</span>
+                        <h3>Virtual Reality dan Contoh Penggunaannya</h3>
+                        <p>Teknologi berkembang semakin pesat setiap saat. Jika kita tidak mengikuti perkembangan
+                            teknologi, yang ada bisa saja kita jadi gaptek alias gagap teknologi. Dari sekian banyak
+                            teknologi yang ada, teknologi VR alias virtual reality sekarang ini sedang banyak
+                            dibicarakan.</p>
+                        <a href="">Read More</a>
+                    </div>
+                </div>
+                <div class="right" data-aos="fade-right" data-aos-delay="100" data-aos-duration="1000">
+                    <div class="wrapper">
+                        <div class="thumb">
+                            <img src="img/market.jpg" alt="">
+                        </div>
+                        <div class="content">
+                            <span>Minggu, 08 Agu 2021 08:10 WIB</span>
+                            <h4>Pasar adalah Tempat Orang Berjual Beli, Kenali Jenis dan Ciri-cirinya </h4>
+                        </div>
+                    </div>
+
+                    <div class="wrapper">
+                        <div class="thumb">
+                            <img src="img/MU.jpg" alt="">
+                        </div>
+                        <div class="content">
+                            <span>Minggu, 08 Agu 2021 08:10 WIB</span>
+                            <h4>Jerman Menang 4-0 atas Makedonia Utara, Werner Akui Sempat Kesulitan</h4>
+                        </div>
+                    </div>
+
+                    <div class="wrapper">
+                        <div class="thumb">
+                            <img src="img/teach.jpg" alt="">
+                        </div>
+                        <div class="content">
+                            <span>Minggu, 08 Agu 2021 08:10 WIB</span>
+                            <h4>Cara Bangun Motivasi Belajar Anak Berkebutuhan Khusus Selama PJJ</h4>
+                            <!-- <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit...</p>
+                            <a href="">10 Comments</a> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Special Topic -->
+        <div class="topic-container" data-aos="fade-up-right" data-aos-duration="2000">
+            <div class="blog">
+                <h3 class="heading-topic">Special Topic</h3>
+                <div class="blog-post">
+                    <div class="thumbnail">
+                        <img src="img/spiderman.jpg" alt="">
+                    </div>
+                    <div class="blog-content">
+                        <span>Entertainment</span>
+                        <h3>Spider-Man No Way Home Trending Topic di Twitter Usai Teaser Trailer Dirilis</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius dicta atque neque
+                            voluptatem accusantium aliquid eum sunt officiis cum illo!</p>
+                        <span class="topic">Sabtu, 07 Agu 2021 09:10 WIB</span>
+                    </div>
+                </div>
+
+                <div class="blog-post">
+                    <div class="thumbnail">
+                        <img src="img/IG.jpg" alt="">
+                    </div>
+                    <div class="blog-content">
+                        <span>Social Media</span>
+                        <h3>Instagram Down, Netizen di Dunia Curhat di Twitter</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius dicta atque neque
+                            voluptatem accusantium aliquid eum sunt officiis cum illo!</p>
+                        <span class="topic">Sabtu, 07 Agu 2021 09:10 WIB</span>
+                    </div>
+                </div>
+
+                <div class="blog-post">
+                    <div class="thumbnail">
+                        <img src="img/demonstration.jpg" alt="">
+                    </div>
+                    <div class="blog-content">
+                        <span>Government</span>
+                        <h3>Demo Protes Kartu Sehat Presiden Macron</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius dicta atque neque
+                            voluptatem accusantium aliquid eum sunt officiis cum illo!</p>
+                        <span class="topic">Sabtu, 07 Agu 2021 09:10 WIB</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Section Video -->
+        <div class="container">
+            <div class="top" data-aos="fade-down" data-aos-duration="1000">
+                <h3 class="heading-category">Video</h3>
+                <div class="left">
+                    <div class="thum">
+                        <img src="img/polutions.jpg" alt="" class="image-top">
+                    </div>
+                </div>
+                <div class="right">
+                    <div class="blog-content">
+                        <span>Government</span>
+                        <h3>Polusi Udara yang ‘menghilangkan’ Teluk Jakarta dan Gugatan Warga</h3>
+                        <p>Ketika kualitas udara baik, Teluk Jakarta dan kapal-kapal yang hilir mudik seharusnya dapat
+                            dilihat dengan jelas. Kondisi yang lebih sering terlihat justru kelabu, dan aplikasi
+                            pemantau kualitas udara menunjukan udara Jakarta sedang tidak sehat.</p>
+                        <span class="topic">Minggu, 08 Agu 2021 08:10 WIB</span>
+                    </div>
+                </div>
+            </div>
+            <div class="bottom" data-aos="fade-up-right" data-aos-duration="2000">
+                <div class="blog-video">
+                    <div class="thumbnail">
+                        <img src="img/destination.jpg" alt="">
+                    </div>
+                    <div class="blog-content">
+                        <span>Travel</span>
+                        <h3>Damar Langit, Tempat Wisata Baru di Puncak Bogor Berpanorama Pegunungan</h3>
+                    </div>
+                </div>
+                <div class="blog-video">
+                    <div class="thumbnail">
+                        <img src="img/electricity.jpg" alt="">
+                    </div>
+                    <div class="blog-content">
+                        <span>Government</span>
+                        <h3>Memasok Listrik ke Pulau Terpencil: Bisakah Ebergi Bersih Jadi Solusi?</h3>
+                    </div>
+                </div>
+                <div class="blog-video">
+                    <div class="thumbnail">
+                        <img src="img/usa.jpg" alt="">
+                    </div>
+                    <div class="blog-content">
+                        <span>Government</span>
+                        <h3>Taliban wanti-wanti AS Tak Intervensi Konflik Afghanistan</h3>
+                    </div>
+                </div>
+                <div class="blog-video">
+                    <div class="thumbnail">
+                        <img src="img/destination.jpg" alt="">
+                    </div>
+                    <div class="blog-content">
+                        <span>Travel</span>
+                        <h3>Damar Langit, Tempat Wisata Baru di Puncak Bogor Berpanorama Pegunungan</h3>
+                    </div>
+                </div>
+                <div class="blog-video">
+                    <div class="thumbnail">
+                        <img src="img/electricity.jpg" alt="">
+                    </div>
+                    <div class="blog-content">
+                        <span>Government</span>
+                        <h3>Memasok Listrik ke Pulau Terpencil: Bisakah Ebergi Bersih Jadi Solusi?</h3>
+                    </div>
+                </div>
+                <div class="blog-video">
+                    <div class="thumbnail">
+                        <img src="img/usa.jpg" alt="">
+                    </div>
+                    <div class="blog-content">
+                        <span>Government</span>
+                        <h3>Taliban wanti-wanti AS Tak Intervensi Konflik Afghanistan</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Section Footer -->
+    <footer>
+        <div class="site-container">
+            <div class="footer-class">
+                <div class="footer-1">
+                    <h3>NewsID.com</h3>
+                    <p>Jln Genteng Raya No 4-5, Surabaya Indonesia</p>
+                    <div class="social-icons">
+                        <i class="icofont-facebook"></i>
+                        <i class="icofont-instagram"></i>
+                        <i class="icofont-twitter"></i>
+                        <i class="icofont-linkedin"></i>
+                    </div>
+                </div>
+                <div class="footer-2">
+                    <h3>Support</h3>
+                    <p>Account</p>
+                    <p>Legal</p>
+                    <p>Afilliate Progam</p>
+                    <p>Contact</p>
+                    <p>Stories</p>
+                </div>
+                <div class="footer-3">
+                    <h3>Service</h3>
+                    <p>Featured</p>
+                    <p>Pricing</p>
+                    <p>Free Masterclass</p>
+                    <p>Compare Us</p>
+                    <p>Careers</p>
+                </div>
+                <div class="footer-4">
+                    <h3>About</h3>
+                    <p>Product</p>
+                    <p>Resource</p>
+                    <p>Blog</p>
+                    <p>Stories</p>
+                    <p>FAQ</p>
+                </div>
+            </div>
+            <div class="copyright">
+                <h4>&copy; Copyright 2021 Sastra R</h4>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
+    <script>
+        const mobileMenu = document.querySelector('.mobile-menu');
+        // console.log(mobileMenu);
+        const menu = document.querySelector('.menu');
+        // console.log(menu);
+        mobileMenu.addEventListener('click', function() {
+            menu.classList.toggle('show');
+        })
+    </script>
+</body>
+
+</html>
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    {{-- Navbar --}}
+    @include('partials.navbar')
+
+    <div class="container mt-4">
+        @yield('container')
+    </div>
+
+
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+</body>
+
+</html>
