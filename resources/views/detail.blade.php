22484e9579b95a638e6be4e33d1b017f1fc45ec1
sastra12 <anonymousonly594@gmail.com>
2022-07-03 08:40:08 +0700

    fix fitur upload file

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -5,8 +5,14 @@
     <p>By, <a href="/?author={{ $detail->author->username }}">{{ $detail->author->username }}</a> in <a
             href="/?category={{ $detail->category->slug }}">{{ $detail->category->name }}</a>
     </p>
-    <img class="img-fluid" src="https://source.unsplash.com/1200x400?{{ $detail->category->name }}"
-        alt="Card image cap">
+    @if ($detail->image)
+        <div style="max-height: 350px; overflow:hidden;">
+            <img class="img-fluid mt-3" src="{{ asset('storage/' . $detail->image) }}" alt="Card image cap">
+        </div>
+    @else
+        <img class="img-fluid" src="https://source.unsplash.com/1200x400?{{ $detail->category->name }}" alt="Card image cap">
+    @endif
+
     <article class="mt-2 fs-5">
         {!! $detail->body !!}
     </article>
